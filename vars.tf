43380d8d5e93371d7d6ce91cd94459e511d44e8c
mmichel122 <mikael.michelier@googlemail.com>
2021-01-04 10:47:24 +0000

    new token vars

--- vars.tf
+++ vars.tf
@@ -1,5 +1,4 @@
 variable "tfe_token" {
-  #default = "okGjujt0idovZQ.atlasv1.RMkv5CS4s0lbiuZoM8tOrJoxZCFKTGCjtZwgYUhiCza4MhWmRTfI9hJ0HVVzCzRt5WI"
 }
 
 variable "tfe_org_name" {
@@ -7,7 +6,6 @@ variable "tfe_org_name" {
 }
 
 variable "github_token" {
-  #default = "2b128c2cc02899079c0c7ae2bd1d2482979f8fef"
 }
 
 variable "identifier" {
