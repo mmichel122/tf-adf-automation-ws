b6bbcd322dc1044b27755569d43ebb106b2ad781
mmichel122 <mikael.michelier@googlemail.com>
2021-01-04 10:31:04 +0000

    org id removed

--- vars.tf
+++ vars.tf
@@ -2,17 +2,12 @@ variable "tfe_token" {
   #default = "okGjujt0idovZQ.atlasv1.RMkv5CS4s0lbiuZoM8tOrJoxZCFKTGCjtZwgYUhiCza4MhWmRTfI9hJ0HVVzCzRt5WI"
 }
 
-variable "tfe_org_id" {
-  #default     = ""
-  description = "ID of organization in TFE to use, if empty, an organization will be created."
-}
-
 variable "tfe_org_name" {
   #default = ""
 }
 
 variable "github_token" {
-  #default = "ot-EWKx6Hyuk6tXM2No"
+  #default = "7b7da756bbddcdb73481ea420014968814c232a4"
 }
 
 variable "identifier" {
