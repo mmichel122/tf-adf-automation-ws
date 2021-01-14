8ea8432c427b0d92bc71cc8b3a3d0a5cd6109be1
mmichel122 <mikael.michelier@googlemail.com>
2021-01-14 15:47:50 +0000

    test vcs separate resource

--- modules/workspace/main.tf
+++ modules/workspace/main.tf
@@ -2,13 +2,6 @@ terraform {
   required_version  = ">=0.12"
 }
 
-resource "null_resource" "previous" {}
-
-resource "time_sleep" "wait_30_seconds" {
-  depends_on = [null_resource.previous]
-  create_duration = "10s"
-}
-
 resource "tfe_oauth_client" "github" {
   organization      = var.tfe_org_name
   api_url           = "https://api.github.com"
@@ -24,13 +17,6 @@ resource "tfe_workspace" "workspace" {
   operations            = true
   queue_all_runs        = true
   file_triggers_enabled = true
-  vcs_repo {
-    identifier          = var.identifier
-    branch              = "main"
-    ingress_submodules  = false
-    oauth_token_id      = tfe_oauth_client.github.oauth_token_id
-    depends_on          = [time_sleep.wait_30_seconds]
-  }
 }
 
 resource "tfe_variable" "AWS_ACCESS_KEY_ID" {
@@ -68,4 +54,15 @@ resource "tfe_variable" "region" {
   value        = var.region
   category     = "terraform"
   workspace_id = tfe_workspace.workspace.id
+}
+
+resource "tfe_workspace" "repo" {
+  name                  = var.tfe_workspace_name
+  organization          = var.tfe_org_name
+  vcs_repo {
+    identifier          = var.identifier
+    branch              = "main"
+    ingress_submodules  = false
+    oauth_token_id      = tfe_oauth_client.github.oauth_token_id
+  }
 }
