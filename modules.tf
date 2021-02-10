57ea1aaed45bd538eb8bc56c71f0fb4a478f02b7
mmichel122 <mikael.michelier@googlemail.com>
2021-02-10 09:41:21 +0000

    new cec account

--- modules.tf
+++ modules.tf
@@ -23,3 +23,16 @@ module "mm-tf-demo-sky-2" {
   AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
   region                = "eu-west-1"
 }
+
+module "mm-tf-demo-sky-3" {
+  source                = "./modules/workspace"
+  tfe_workspace_name    = "mm-demo-sky-03"
+  tfe_workspace_team    = "dta"
+  tfe_token             = var.tfe_token
+  tfe_org_name          = var.tfe_org_name
+  github_token          = var.github_token
+  identifier            = var.identifier
+  AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID
+  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+  region                = "eu-west-1"
+}
